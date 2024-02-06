077687cece13846c5ec379c5ad84b39208053bf8
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2024-02-06 11:40:29 +0900

    2839:동적프로그래밍 / 실버4 / 1.5h

--- boj_algorithm/2839_dp_greedy.py
+++ boj_algorithm/2839_dp_greedy.py
@@ -3,7 +3,7 @@ n = target // 5 # 5로 나눌 몫
 m = target // 3
 lst = []
 
-for i in range(n, -1, -1):
+for i in range(n, -1, -1):  # 5로 먼저 나눠야 하기 때문에 바깥쪽 for문은 5로 설정
     for j in range(m, -1, -1):
         newtarget = 5 * i + 3 * j
         if newtarget == target:
