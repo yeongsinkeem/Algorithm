a3ea57ba3271ba0ce79472a8c66aa848e03cd913
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2024-02-19 01:28:13 +0900

    1018: BF / 실버4 / 1h -> 못 풂

--- boj_algorithm/1018_chessBF.py
+++ boj_algorithm/1018_chessBF.py
@@ -1,21 +1,29 @@
-def chess(A):
-    length = len(A)
-    count = 0
-    for i in range(length - 1):
-        if A[i] == A[i+1]:
-            if A[i+1] == "B":
-                A[i+1] = "W"
-            else:
-                A[i+1] = "B"
-            count += 1
-    return count
+import sys
+N, M = map(int, sys.stdin.readline().split())
+chess = []
 
-N, M = map(int, input().split())
-#sum = 0
-for i in range(N):
-    for j in range(M):
+for _ in range(N):
+    chess.append(list(sys.stdin.readline().strip())) # 행마다 하나의 리스트로
 
-    #lst = list(input())
-    #sum += chess(lst)
+answer = []
+# 8 * 8 크기의 체스판 움직이기
+for row in range(N - 7):
+    for col in range(M - 7):
+        sb = 0  # 시작 요소가 검정색
+        sw = 0  # 시작 요소가 흰색
+        for i in range(row, row + 8):
+            for j in range(col, col + 8):
+                if ( i + j ) % 2 == 0:  # 1, 3, 5, 7, 9 .. 가 검정색
+                    if chess[i][j] == "B":
+                        sw += 1
+                    else :
+                        sb += 1
+                else :
+                    if chess[i][j] == "B":
+                        sb += 1
+                    else :
+                        sw += 1
+        answer.append(sb)
+        answer.append(sw)
+print(min(answer))
 
-#print(sum)
