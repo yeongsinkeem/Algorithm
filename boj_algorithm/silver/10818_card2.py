2720d46305f2805ddf785ba4cf413854c6a03815
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2024-02-22 20:50:06 +0900

    make new file

---
+++ boj_algorithm/silver/10818_card2.py
@@ -0,0 +1,9 @@
+import sys
+from sys import stdin
+N = int(stdin.readline())
+A = list(sys.stdin.readline().split())
+M = int(stdin.readline())
+B = list(sys.stdin.readline().split())
+
+for i in range(M):
+    for j in range(N):
