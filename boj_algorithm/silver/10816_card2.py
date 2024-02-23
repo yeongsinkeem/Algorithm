9ea3e7417ea6a2ca56395ba17af4c313ee292d9b
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2024-02-23 22:54:30 +0900

    10816: 정렬 + 탐색/ 실버4 / 3h -> 시간 초과 해결 !

--- boj_algorithm/silver/10816_card2.py
+++ boj_algorithm/silver/10816_card2.py
@@ -1,46 +1,20 @@
 import sys
-import bisect
-from sys import stdin
-from collections import deque
+from collections import defaultdict
 
-N = int(stdin.readline())
+N = int(sys.stdin.readline())
 A = list(map(int, sys.stdin.readline().split()))
-M = int(stdin.readline())
-B = list(map(int, sys.stdin.readline().split()))   # 탐색해야 하는 카드
+M = int(sys.stdin.readline())
+B = list(map(int, sys.stdin.readline().split()))
 
-def radix_sort(nums):
-    buckets = [deque() for _ in range(10)]
+A.sort()
+Adic = defaultdict(int)
 
-    max_val = max(nums)
-    Q = deque(nums)
-    cur_ten = 1
-
-    while max_val >= cur_ten:
-        while Q:
-            num = Q.popleft()
-            buckets[(num // cur_ten) % 10].append(num)
-
-        for bucket in buckets:
-            while bucket:
-                Q.append(bucket.popleft())
-
-        cur_ten += 10
-
-    return list(Q)
-
-A = radix_sort(A)    # 상근이가 가지고 있는 카드
-Adic = {}
-
+for num in A:
+    Adic[num] += 1 # Adic으로 개수 세어주고
+result = []
 for num in B:
-    count = 0
-    index = bisect.bisect_left(A, num)
-    while index < len(A) and A[index] == num:
-        count += 1
-        index += 1
-    Adic[num] = count
+    result.append(Adic[num])
 
-for num in Adic:
-    print(Adic[num], end = " ")
-#print(Adic)
+print(*result)
 
 
