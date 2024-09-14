b4d324df78b03d72210358a9452d3ad7455aba8c
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2024-09-15 01:18:06 +0900

    2018 카카오 블라인드 채용(54%)

--- programmers/lv1/20.java
+++ programmers/lv1/20.java
@@ -2,29 +2,40 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int solution(String dartResult) {
-        int[] point = new int[4];
+        int []numArr = new int[4];
+        int answer;
+        int idx = 1;
 
-        for (int i = 0, index = 1; i < dartResult.length(); i++){
-            char ch = dartResult.charAt(i);
-
-            if (point[index - 1] == 1 && ch - '0' == 0){
-                point[index - 1] *= 10;
-                continue;
-            } else if (0 <= ch - '0' && ch - '0' <= 9){
-                point[index++] = ch - '0';
+        // 1. 반복문으로 순회하면서 numArr에 숫자 저장하기
+        for(int i = 0; i < dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+            // 2. 숫자 처리
+            if ( numArr[idx-1] == 1 && c == '0' ) {
+                // 들어올 숫자가 10이라면 그에 맞는 처리
+                numArr[idx-1] = 10;
                 continue;
             }
-
-            if (ch == 'D') {
-                point[index - 1] = (int) Math.pow(point[index - 1], 2);
-            } else if (ch == 'T') {
-                point[index - 1] = (int) Math.pow(point[index - 1], 3);
-            } else if (ch == '*'){
-                point[index - 1] *= 2;
-                point[index - 2] *= 2;
-            } else if (ch == '#') point[index - 1] *= -1;
+            else if ( c - '0' >= 0 && c - '0' <= 9 ) {
+                numArr[idx] = c - '0';
+                idx ++;
+                continue;
+            }
+            // 3. 문자 처리
+            if ( c == 'D' ) {
+                numArr[idx - 1] = (int) Math.pow(numArr[idx-1], 2);
+            }
+            else if ( c == 'T') {
+                numArr[idx - 1] = (int) Math.pow(numArr[idx-1], 3);
+            }
+            else if ( c == '*') {
+                numArr[idx - 1] *= 2;
+                numArr[idx - 2] *= 2;
+            }
+            else if ( c == '#') {
+                numArr[idx - 1] *= -1;
+            }
         }
-
-        return IntStream.of(point).sum();
+        answer = IntStream.of(numArr).sum();
+        return answer;
     }
 }
