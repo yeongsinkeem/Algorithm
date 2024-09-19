2420c05d0e659bdb0b0085dafc7a30e948b3e6ae
yeongsinkeem <dudtls0522@sookmyung.ac.kr>
2024-09-19 22:45:24 +0900

    덧칠하기(63%)

---
+++ programmers/lv1/29.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int distance = 0; // 칠한 면적
+
+        for (int i = 0; i < section.length; i++) {
+            if (section[i] <= distance) {
+                continue;
+            }
+            distance = section[i] + m - 1;
+            answer ++;
+        }
+
+        return answer;
+    }
+    }
